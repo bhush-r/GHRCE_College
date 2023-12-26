ed2212a56b6be275b5d4ee3f08a08c74196a94ee
bhush-r <bhushanlilhare956@gmail.com>
2023-12-26 18:27:51 +0530

    add proges bar and page number

--- app/src/main/java/bhushan/org/GHRCEUSER/ebook/PdfViewerActivity.java
+++ app/src/main/java/bhushan/org/GHRCEUSER/ebook/PdfViewerActivity.java
@@ -1,11 +1,87 @@
+//package bhushan.org.GHRCEUSER.ebook;
+//
+//import android.os.AsyncTask;
+//import android.os.Bundle;
+//
+//import androidx.appcompat.app.AppCompatActivity;
+//
+//import com.github.barteksc.pdfviewer.PDFView;
+//import com.github.barteksc.pdfviewer.scroll.DefaultScrollHandle;
+//
+//import java.io.BufferedInputStream;
+//import java.io.IOException;
+//import java.io.InputStream;
+//import java.net.URL;
+//
+//import javax.net.ssl.HttpsURLConnection;
+//
+//import bhushan.org.GHRCEUSER.R;
+//
+//public class PdfViewerActivity extends AppCompatActivity {
+//
+//    private String url;
+//    private PDFView pdfView;
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_pdf_viewer);
+//
+//        url = getIntent().getStringExtra("pdfUri");
+//
+//        pdfView = findViewById(R.id.pdfView);
+//
+//        new PdfDownload().execute(url);
+//    }
+//    private class PdfDownload extends AsyncTask<String,Void, InputStream>{
+//
+//
+//        @Override
+//        protected InputStream doInBackground(String... strings) {
+//            InputStream inputStream = null;
+//
+//            try {
+//                URL url = new URL(strings[0]);
+//                HttpsURLConnection urlConnection = (HttpsURLConnection) url.openConnection();
+//
+//                if (urlConnection.getResponseCode() == 200){
+//                    inputStream = new BufferedInputStream(urlConnection.getInputStream());
+//                }
+//            } catch (IOException e) {
+//                e.printStackTrace();
+//            }
+//
+//            return inputStream;
+//        }
+//
+//        @Override
+//        protected void onPostExecute(InputStream inputStream) {
+//            pdfView.fromStream(inputStream)
+////                    .onPageChange(new OnPageChangeListener() {
+////                        @Override
+////                        public void onPageChanged(int page, int pageCount) {
+////                            // Update UI with the current page number
+////                            setTitle("Page " + (page + 1) + " of " + pageCount);
+////                        }
+////                    })
+//                    .load();
+//        }
+//    }
+//}
+
+
 package bhushan.org.GHRCEUSER.ebook;
 
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+import android.widget.ProgressBar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.github.barteksc.pdfviewer.PDFView;
+import com.github.barteksc.pdfviewer.scroll.DefaultScrollHandle;
 
 import java.io.BufferedInputStream;
 import java.io.IOException;
@@ -20,20 +96,36 @@ public class PdfViewerActivity extends AppCompatActivity {
 
     private String url;
     private PDFView pdfView;
+    private ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // Add the line to prevent screenshots and screen recordings
+        getWindow().setFlags(
+                WindowManager.LayoutParams.FLAG_SECURE,
+                WindowManager.LayoutParams.FLAG_SECURE
+        );
+
         setContentView(R.layout.activity_pdf_viewer);
 
         url = getIntent().getStringExtra("pdfUri");
 
         pdfView = findViewById(R.id.pdfView);
+        progressBar = findViewById(R.id.progressBar);
 
         new PdfDownload().execute(url);
     }
-    private class PdfDownload extends AsyncTask<String,Void, InputStream>{
 
+    private class PdfDownload extends AsyncTask<String, Void, InputStream> {
+
+        @Override
+        protected void onPreExecute() {
+            // Show the progress bar before starting the download
+            progressBar.setVisibility(ProgressBar.VISIBLE);
+            pdfView.setVisibility(PDFView.INVISIBLE);
+        }
 
         @Override
         protected InputStream doInBackground(String... strings) {
@@ -43,7 +135,7 @@ public class PdfViewerActivity extends AppCompatActivity {
                 URL url = new URL(strings[0]);
                 HttpsURLConnection urlConnection = (HttpsURLConnection) url.openConnection();
 
-                if (urlConnection.getResponseCode() == 200){
+                if (urlConnection.getResponseCode() == 200) {
                     inputStream = new BufferedInputStream(urlConnection.getInputStream());
                 }
             } catch (IOException e) {
@@ -55,15 +147,26 @@ public class PdfViewerActivity extends AppCompatActivity {
 
         @Override
         protected void onPostExecute(InputStream inputStream) {
-            pdfView.fromStream(inputStream)
-//                    .onPageChange(new OnPageChangeListener() {
-//                        @Override
-//                        public void onPageChanged(int page, int pageCount) {
-//                            // Update UI with the current page number
-//                            setTitle("Page " + (page + 1) + " of " + pageCount);
-//                        }
-//                    })
-                    .load();
+            // Introduce a delay of 2 seconds before hiding the progress bar
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    // Hide the progress bar after the delay
+                    progressBar.setVisibility(ProgressBar.GONE);
+
+                    // Show the PDFView
+                    pdfView.setVisibility(PDFView.VISIBLE);
+
+                    // Load the PDF into the PDFView
+                    pdfView.fromStream(inputStream)
+                            .defaultPage(0) // Set default page to 0
+                            .scrollHandle(new DefaultScrollHandle(PdfViewerActivity.this)) // Add scroll handle
+                            .load();
+                }
+            }, 8000); // Delay in milliseconds (adjust as needed)
         }
     }
 }
+
+
+
--- app/src/main/java/bhushan/org/GHRCEUSER/paper/PaperViewerActivity.java
+++ app/src/main/java/bhushan/org/GHRCEUSER/paper/PaperViewerActivity.java
@@ -2,10 +2,14 @@ package bhushan.org.GHRCEUSER.paper;
 
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+import android.widget.ProgressBar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.github.barteksc.pdfviewer.PDFView;
+import com.github.barteksc.pdfviewer.scroll.DefaultScrollHandle;
 
 import java.io.BufferedInputStream;
 import java.io.IOException;
@@ -15,26 +19,41 @@ import java.net.URL;
 import javax.net.ssl.HttpsURLConnection;
 
 import bhushan.org.GHRCEUSER.R;
+import bhushan.org.GHRCEUSER.ebook.PdfViewerActivity;
 
 public class PaperViewerActivity extends AppCompatActivity {
 
     private String url;
     private PDFView paperView;
 
+    private ProgressBar progressBar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // Add the line to prevent screenshots and screen recordings
+        getWindow().setFlags(
+                WindowManager.LayoutParams.FLAG_SECURE,
+                WindowManager.LayoutParams.FLAG_SECURE
+        );
+
         setContentView(R.layout.activity_paper_viewer);
 
         url = getIntent().getStringExtra("paperUri");
 
         paperView = findViewById(R.id.paperView);
+        progressBar = findViewById(R.id.progressBar);
 
         new PaperDownload().execute(url);
     }
     private class PaperDownload extends AsyncTask<String,Void, InputStream>{
 
-
+        @Override
+        protected void onPreExecute() {
+            // Show the progress bar before starting the download
+            progressBar.setVisibility(ProgressBar.VISIBLE);
+            paperView.setVisibility(PDFView.INVISIBLE);
+        }
         @Override
         protected InputStream doInBackground(String... strings) {
             InputStream inputStream = null;
@@ -55,15 +74,23 @@ public class PaperViewerActivity extends AppCompatActivity {
 
         @Override
         protected void onPostExecute(InputStream inputStream) {
-            paperView.fromStream(inputStream)
-//                    .onPageChange(new OnPageChangeListener() {
-//                        @Override
-//                        public void onPageChanged(int page, int pageCount) {
-//                            // Update UI with the current page number
-//                            setTitle("Page " + (page + 1) + " of " + pageCount);
-//                        }
-//                    })
-                    .load();
+            // Introduce a delay of 2 seconds before hiding the progress bar
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    // Hide the progress bar after the delay
+                    progressBar.setVisibility(ProgressBar.GONE);
+
+                    // Show the PDFView
+                    paperView.setVisibility(PDFView.VISIBLE);
+
+                    // Load the PDF into the PDFView
+                    paperView.fromStream(inputStream)
+                            .defaultPage(0) // Set default page to 0
+                            .scrollHandle(new DefaultScrollHandle(PaperViewerActivity.this)) // Add scroll handle
+                            .load();
+                }
+            }, 5000); // Delay in milliseconds (adjust as needed)
         }
     }
 }
